902d2fa090b3652d4b9b8cd4133ef46be81e37f5
rahadiankr <71833359+rahadiankr@users.noreply.github.com>
2022-02-11 18:22:43 +0700

    hapus timer

--- account.php
+++ account.php
@@ -303,7 +303,7 @@ function frmreset(){
     }
 var countdownTimer = setInterval(\'secondPassed()\', 1000);
 </script>';
-                                echo '<font size="3" style="margin-left:100px;font-family:\'typo\' font-size:20px; font-weight:bold;color:darkred">Time Left : </font><span class="timer btn btn-default" style="margin-left:20px;"><font style="font-family:\'typo\';font-size:20px;font-weight:bold;color:darkblue" id="countdown"></font></span><span class="timer btn btn-primary" style="margin-left:50px" onclick="end()"><span class=" glyphicon glyphicon-off"></span>&nbsp;&nbsp;<font style="font-size:12px;font-weight:bold">Finish Quiz</font></span>';
+                                echo '<span class="timer btn btn-primary" style="margin-left:50px" onclick="end()"><span class=" glyphicon glyphicon-off"></span>&nbsp;&nbsp;<font style="font-size:12px;font-weight:bold">Finish Quiz</font></span>';
                                 $eid   = @$_GET['eid'];
                                 $sn    = @$_GET['n'];
                                 $total = @$_GET['t'];
@@ -312,7 +312,7 @@ var countdownTimer = setInterval(\'secondPassed()\', 1000);
                                 while ($row = mysqli_fetch_array($q)) {
                                     $qns = stripslashes($row['qns']);
                                     $qid = $row['qid'];
-                                    echo '<b><pre style="background-color:white" onmousedown="return false" onselectstart="return false"><div style="white-space: normal; font-size:20px;font-weight:bold;font-family:calibri;margin:10px" onmousedown="return false" onselectstart="return false">' . $sn . ' : ' . $qns . '</div></pre></b>';
+                                    echo '<b><pre style="background-color:white" onmousedown="return false" onselectstart="return false"><div style="white-space: normal; font-size:20px;font-weight:bold;font-family:calibri;margin:10px" onmousedown="return false" onselectstart="return false">' . $sn . ' .) ' . $qns . '</div></pre></b>';
                                 }
 
                                 echo '<form id="qform" action="update.php?q=quiz&step=2&eid=' . $eid . '&n=' . $sn . '&t=' . $total . '&qid=' . $qid . '" method="POST"  class="form-horizontal">
